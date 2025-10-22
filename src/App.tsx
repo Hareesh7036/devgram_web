9dedb6f7dccc05d25b728cbb6eb5d4f1d74331a6
Hareesh7036 <madahareesh12@gmail.com>
2025-10-22 09:18:52 +0530

    feat: implement login page and integrte login api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Body from "./components/body";
+import Login from "./components/login";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Body />}>
             <Route path="home" element={<div>Home</div>} />
-            <Route path="login" element={<div>Login</div>} />
+            <Route path="login" element={<Login />} />
             <Route path="signup" element={<div>Sign Up</div>} />
             <Route path="*" element={<div>404 Not Found</div>} />
           </Route>
