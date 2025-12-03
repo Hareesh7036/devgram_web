9726a106607eaa7127b878ea5b458a8e405c5a08
Hareesh7036 <madahareesh12@gmail.com>
2025-12-03 22:27:47 +0530

    update url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:8080" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:8080"
+    : "https://devgram-6l12.onrender.com";
